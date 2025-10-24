1ba943f7f9a69c5ada578447e481e07e8a84c1fb
AshmitSherigar <sherigarashmit@gmail.com>
2025-10-24 16:29:10 +0530

    feat : added simple navigation bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
+import { Route , Routes } from "react-router-dom"
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<>Home</>}/>
+        <Route path="/compress-file" element={<>Compress</>}/>
+        <Route path="/convert-file" element={<>Convert</>}/>
+        <Route path="/merge-file" element={<>Merge</>}/>
+        <Route path="/combine-file" element={<>Combine</>}/>
+        <Route path="/edit-file" element={<>Edit</>}/>
+      </Routes>
+    </div>
   )
 }
 
