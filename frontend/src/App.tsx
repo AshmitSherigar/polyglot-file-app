52947f35864b261466643906c99cd08c797c8de4
Ashmit Sherigar <128444050+AshmitSherigar@users.noreply.github.com>
2025-10-24 23:00:27 +0530

    added frontend of drag and drop files with preview.
    This PR adds frontend of drag and drop files with preview with delete of the file also.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { Route , Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
-
+import Combine from "./page/Combine"
 const App = () => {
   return (
     <div>
@@ -10,7 +10,7 @@ const App = () => {
         <Route path="/compress-file" element={<>Compress</>}/>
         <Route path="/convert-file" element={<>Convert</>}/>
         <Route path="/merge-file" element={<>Merge</>}/>
-        <Route path="/combine-file" element={<>Combine</>}/>
+        <Route path="/combine-file" element={<Combine/>}/>
         <Route path="/edit-file" element={<>Edit</>}/>
       </Routes>
     </div>
